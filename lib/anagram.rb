b12b53670100256ae2fb093b312697f511055f40
Jason Baik <baikjs87@gmail.com>
2022-07-19 01:06:36 -0400

    commit tree f3f41924008b562a3b09aa66487b5e882bfe0d37

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match word
+        if word.sort == @word.sort
+            return word
+        end
+    end
+end
