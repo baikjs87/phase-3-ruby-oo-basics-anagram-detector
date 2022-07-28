6e1ff5a8a2bf4780ae795c33248b769e6c35d8fe
Jason Baik <baikjs87@gmail.com>
2022-07-28 01:36:49 -0400

    Complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        array.select do |data|
+            if @word == data
+                data
+            elsif @word.chars.sort == data.chars.sort
+                data
+            end
+        end
+    end
+end
